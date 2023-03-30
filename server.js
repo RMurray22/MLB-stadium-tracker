adc6e051fdba36e81e48961d4ddb8d057ea14854
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-03-29 20:15:36 -0700

    created schema code and finished server.js and connection.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const path = require("path");
 const express = require("express");
-const session = require("express-sessions");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const helpers = require("./utils/helpers");
+//const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const app = espress();
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const sess = {
   secret: "secret squirrel",
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
+//app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
