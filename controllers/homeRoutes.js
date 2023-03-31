2b1ccbfc25ed116d852163c436660d1ad0604b53
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-03-30 21:44:39 -0700

    created userRoutes and homeRoutes, moved handlebar except main out of layout and into view, updated index.js in models, updated index.js in api and controllers

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const { User } = require("../models");
+//const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ["password"] },
+      order: [["user_email", "ASC"]],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render("homepage", {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,17 +1,18 @@
-const Stadium = require('./stadium');
-const User = require('./user');
-const Team = require('./team');
+const Stadium = require("./stadium");
+const User = require("./user");
+const Team = require("./team");
 
 Team.hasOne(Stadium, {
-    foreignKey: 'stadium_id',
+  foreignKey: "stadium_id",
 });
 Stadium.belongsTo(Team, {
-    foreignKey: 'stadium_id',
+  foreignKey: "stadium_id",
 });
 
 User.hasOne(Team, {
-    foreignKey: 'user_favorite_team',
+  foreignKey: "user_favorite_team",
 });
 Team.hasMany(User, {
-    foreignKey: 'user_favorite_team',
+  foreignKey: "user_favorite_team",
 });
+module.exports = { Stadium, User, Team };
