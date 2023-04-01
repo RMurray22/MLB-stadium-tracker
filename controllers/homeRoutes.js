f49a991ed7ccf2be10ed5fa0498a800397cb64e7
Michael Welle <michaelpwelle@gmail.com>
2023-04-01 16:13:03 -0700

    worked on front end, login working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const { User } = require("../models");
-//const withAuth = require("../utils/auth");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
     const userData = await User.findAll({
       attributes: { exclude: ["password"] },
-      order: [["user_email", "ASC"]],
+      order: [["username", "ASC"]],
     });
 
     const users = userData.map((project) => project.get({ plain: true }));
@@ -28,6 +28,26 @@ router.get("/login", (req, res) => {
   }
 
   res.render("login");
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Team }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,46 +1,55 @@
-const newFormHandler = async (event) => {
-    event.preventDefault();
+// const newFormHandler = async (event) => {
+//     event.preventDefault();
   
-    const name = document.querySelector('#stadium-name').value.trim();
-    const favoriteTeam = document.querySelector('#favorite-team').value.trim();
+//     const name = document.querySelector('#stadium-name').value.trim();
+//     const favoriteTeam = document.querySelector('#favorite-team').value.trim();
   
-    if (name && favoriteTeam) {
-      const response = await fetch(`/api/projects`, {
-        method: 'POST',
-        body: JSON.stringify({ name, favoriteTeam }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+//     if (name && favoriteTeam) {
+//       const response = await fetch(`/api/projects`, {
+//         method: 'POST',
+//         body: JSON.stringify({ name, favoriteTeam }),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       });
   
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create stadium');
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert('Failed to create stadium');
+//       }
+//     }
+//   };
   
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+//   const delButtonHandler = async (event) => {
+//     if (event.target.hasAttribute('data-id')) {
+//       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/projects/${id}`, {
-        method: 'DELETE',
-      });
+//       const response = await fetch(`/api/projects/${id}`, {
+//         method: 'DELETE',
+//       });
   
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete stadium');
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert('Failed to delete stadium');
+//       }
+//     }
+//   };
   
-  document
-    .querySelector('.new-stadium-form')
-    .addEventListener('submit', newFormHandler);
+//   document
+//     .querySelector('.new-stadium-form')
+//     .addEventListener('submit', newFormHandler);
   
-  document
-    .querySelector('.stadium-list')
-    .addEventListener('click', delButtonHandler);
+//   document
+//     .querySelector('.stadium-list')
+//     .addEventListener('click', delButtonHandler);
+
+const teams = ["Minnesota Twins", "New York Yankees", "Los Angeles Dodgers"];
+
+window.onload = function() {
+  var favoriteSel = document.getElementById("favorite-team");
+  for (let x in teams) {
+    favoriteSel.options[favoriteSel.options.length] = new Option(x, x);
+  }
+}
