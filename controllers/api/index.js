f092a141171128237db52328df5ef1d1a164c5a3
fdirige <118780727+fdirige@users.noreply.github.com>
2023-04-02 11:00:05 -0700

    pulling updates

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-<<<<<<< HEAD
 // const projectRoutes = require("./projectRoutes");
 
 router.use("/users", userRoutes);
 // router.use("/projects", projectRoutes);
-=======
 
 router.use("/users", userRoutes);
->>>>>>> 38d5cf5aee74f2d1797fb77a36679c461376c8c6
+
 
 module.exports = router;
